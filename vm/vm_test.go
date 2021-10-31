f0af8301fccaaad51899d54e3492f52029b4d6ca
yourfavoritedev <christopherngo94@gmail.com>
2021-10-30 20:47:39 -0700

    Uncomment tests

--- vm/vm_test.go
+++ vm/vm_test.go
@@ -229,9 +229,9 @@ func TestStringExpressions(t *testing.T) {
 
 func TestArrayLiterals(t *testing.T) {
 	tests := []vmTestCase{
-		// {"[]", []int{}},
+		{"[]", []int{}},
 		{"[1, 2, 3]", []int{1, 2, 3}},
-		// {"[1 + 2, 3 * 4, 5 + 6]", []int{3, 12, 11}},
+		{"[1 + 2, 3 * 4, 5 + 6]", []int{3, 12, 11}},
 	}
 
 	runVmTests(t, tests)
