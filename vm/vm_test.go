4c7b688e50d3590daac594c80125371f49a7c968
yourfavoritedev <christopherngo94@gmail.com>
2021-11-04 02:45:27 -0700

    Compile and execute function calls

--- vm/vm_test.go
+++ vm/vm_test.go
@@ -302,3 +302,56 @@ func TestIndexExpressions(t *testing.T) {
 
 	runVmTests(t, tests)
 }
+
+func TestCallingFunctionsWithoutArguments(t *testing.T) {
+	tests := []vmTestCase{
+		{
+			input: `
+		let fivePlusTen = fn() { 5 + 10; };
+		let x = 5
+		fivePlusTen();
+		`,
+			expected: 15,
+		},
+		{
+			input: `
+		let one = fn() { 1; };
+		let two = fn() { 2; };
+		one() + two()
+		`,
+			expected: 3,
+		},
+		{
+			input: `
+		let a = fn() { 1; };
+		let b = fn() { a() + 1 };
+		let c = fn() { b() + 1 };
+		c();
+		`,
+			expected: 3,
+		},
+	}
+
+	runVmTests(t, tests)
+}
+
+func TestFunctionsWithReturnStatement(t *testing.T) {
+	tests := []vmTestCase{
+		{
+			input: `
+		let earlyExit = fn() { return 99; 100; };
+		earlyExit();
+		`,
+			expected: 99,
+		},
+		{
+			input: `
+		let earlyExit = fn() { return 99; return 100; };
+		earlyExit();	
+		`,
+			expected: 99,
+		},
+	}
+
+	runVmTests(t, tests)
+}
